1d02af8c84073f4d7d1ddff61d2f422b5d8dfafc
prongs95 <not007@hackermail.com>
2017-01-14 13:02:17 -0600

    forgot to add this in the last commit

--- src/org/usfirst/frc/team5275/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team5275/robot/subsystems/DriveTrain.java
@@ -1,6 +1,7 @@
 
 package org.usfirst.frc.team5275.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -10,11 +11,12 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class DriveTrain extends Subsystem {
-    public Talon LF;
+    /*public Talon LF;
     public Talon RF;
     public Talon LB;
-    public Talon RB;
-    public Talon Collector = new Talon(5);
+    public Talon RB;*/
+	public Servo camServo = new Servo(6);
+    public Talon Collector = new Talon(8);
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
     public void initDefaultCommand() {
